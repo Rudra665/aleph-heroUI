51ec23c26562cbf979d9974925f6716b8c2536d5
rudra <bhadouriarudra665@gmail.com>
2025-11-30 00:59:53 +0530

    refactor: clean up ThemeSwitch component by removing unnecessary line breaks

--- components/theme-switch.tsx
+++ components/theme-switch.tsx
@@ -44,10 +44,9 @@ export const ThemeSwitch: FC<ThemeSwitchProps> = ({
         className: clsx(
           "px-px transition-opacity hover:opacity-80 cursor-pointer",
           className,
-          classNames?.base,
+          classNames?.base
         ),
-      })}
-    >
+      })}>
       <VisuallyHidden>
         <input {...getInputProps()} />
       </VisuallyHidden>
@@ -66,10 +65,9 @@ export const ThemeSwitch: FC<ThemeSwitchProps> = ({
               "px-0",
               "mx-0",
             ],
-            classNames?.wrapper,
+            classNames?.wrapper
           ),
-        })}
-      >
+        })}>
         {!isSelected || isSSR ? (
           <SunFilledIcon size={22} />
         ) : (
